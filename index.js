342a3917ab60469f8104463d5223281379c3dd54
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-03-20 00:33:19 +0300

    Поправил комментарии

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
 const carbone = require("carbone");
 
-// Read data to inject
+// Читаем исходные данные
 const data = require("./source.json");
-
-// Render options
-const options = {
-	lang: "ru"
-};
 
 carbone.addFormatters(require("./parser.js"));
 
-// Generate a report using the sample template provided by carbone module
+// И отправляем их в шаблон
 carbone.render("template.docx", data, function(err, result) {
 	if (err) {
 		return console.error(err);
 	}
-	// Write the result
+	// Записываем результат в отдельный файл
 	fs.writeFileSync("result.docx", result);
 });
